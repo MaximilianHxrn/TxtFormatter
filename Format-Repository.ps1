4e2b771c5d816ed6d557b04c4e7ca56a08509c9a
Maximilian Horn <horn@singhammer.com>
2020-11-05 14:37:40 +0100

    Added .ps1 Script; Added SingleFile Function

---
+++ Format-Repository.ps1
@@ -0,0 +1,14 @@
+﻿Function Get-Folder($initialDirectory)
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")
+
+    $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
+    $foldername.rootfolder = "MyComputer"
+
+    if($foldername.ShowDialog() -eq "OK") {
+        $folder += $foldername.SelectedPath
+        Start-Process Formatter.exe $folder
+    }
+
+}
+Get-Folder
