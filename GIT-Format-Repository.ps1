7e2d16d9b5505cc5d900304f0828c82300acb470
Maximilian Horn <horn@singhammer.com>
2020-11-06 15:27:22 +0100

    Added Format Repository for GIT

---
+++ GIT-Format-Repository.ps1
@@ -0,0 +1,21 @@
+﻿param
+(
+    [string] $workspaceFolder
+)
+
+if ($workspaceFolder -eq $null)
+{
+    $workspaceFolder = "`"" + (Get-Item -Path ".\" -Verbose).FullName + "`""
+}
+else
+{
+    Write-Host ''
+    Write-Host 'Please ensure when calling sit-format via GIT Bash that your directory should look like:'
+    Write-Host ''
+    Write-Host '< ''"Your\Path\To\File"'' >'
+    Write-Host ''
+    Write-Host '______________________________'
+    Write-Host ''
+}
+cd 'Z:\Transfer\sit.mh\TxtFormatter'
+.\Formatter.exe $workspaceFolder
